9ec2b98fde50f22d9c63cd73c785d4d96ffd2308
Alexander Moreno Rodriguez <almoreno@eliteflower.com>
2022-03-19 22:12:29 -0500

    Agregadas funciones delete, truncate y decorador @affectedRows en las funciones que aplica modificacion de linea

--- pySQL.py
+++ pySQL.py
@@ -1,33 +1,13 @@
 
 from pylib.mod.utils import roundBy
-import pymssql as db
-import sqlalchemy as sal
+# import pymssql as db
+import sqlalchemy as db
+
 from sqlalchemy import create_engine as sen
 from sqlalchemy.sql import text as sat
 
-# dbProjCon = {
-#     'Test': {
-#         'server': 'elt-dbproj-fca\\testing',
-#         'db': 'Logistica',
-#         'user': 'talend',
-#         'password': 'S3rvT4l3nd*',
-#         'tables': {
-#             'table': {'schema': '',
-#                       'name': ''}
-#         }
-#     }}
-
-
-"""
-TRANSACCIONES SQL
-"""
-
-
-dbProjCon = ''
-
 
 def stringConnect(con):
-
     stringConnect = 'mssql+pymssql://{}:{}@{}/{}'
     return stringConnect.format(
         con['user'],
@@ -38,30 +18,82 @@ def stringConnect(con):
 
 
 def engineCon(con):
-    return sal.create_engine(con)
+    return db.create_engine(con)
 
 
-def insertDataToSql(srtCon, schema, table, data):
-    createTable(srtCon, schema, table, data)
-    data.to_sql(
-        schema=schema,
-        name=table,
-        con=engineCon(srtCon),
-        if_exists='append',
-        index=False
-    )
+def rowCount(strCon, schema, table):
+    try:
+        ifexist = '''IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[{}].[{}]') AND type in (N'U'))\n'''.format(
+            schema, table)
+        rountRows = ifexist + '\tSELECT COUNT(*) rows FROM [{}].[{}];'
+        return engineCon(strCon).execute(
+            sat(
+                rountRows.format(schema, table)
+            ).execution_options(stream_results=True)
+        ).mappings().all()[0]['rows']
+
+    except:
+        return 0
 
 
-def truncateTable(srtCon, schema, table):
-    TruncateTable = 'TRUNCATE TABLE {}.{};'
-    engineCon(srtCon).execute(
+def affectedRows(func):
+    def wrapper(*args, **kwargs):
+        initialrows = rowCount(
+            kwargs['strCon'], kwargs['schema'], kwargs['table'])
+        results = func(*args, **kwargs)
+        finalrows = rowCount(
+            kwargs['strCon'], kwargs['schema'], kwargs['table'])
+        affectedRows = finalrows - initialrows
+        print('{} to {} affectedRows: {}'.format(
+            func.__name__, kwargs['table'], affectedRows))
+        return results
+
+    return wrapper
+
+
+@affectedRows
+def truncateTable(strCon, schema, table):
+    TruncateTable = 'TRUNCATE TABLE [{}].[{}];'
+    engineCon(strCon).execute(
         sat(
             TruncateTable.format(schema, table)
         ).execution_options(autocommit=True)
     )
 
 
-def createTableStament(data, schema='dbo', table='newTable'):
+@affectedRows
+def insertDataToSql(strCon, schema, table, data, truncate=False, index=False):
+    createTable(strCon, schema, table, data, index)
+
+    if_exists = ('append', 'replace')[truncate]
+
+    data.to_sql(
+        schema=schema,
+        name=table,
+        con=engineCon(strCon),
+        if_exists=if_exists,
+        index=False
+    )
+
+
+@affectedRows
+def deleteDataToSql(strCon, schema, table, where=[]):
+    deleteData = ''
+    for w in where:
+        if deleteData == '':
+            deleteData += 'WHERE ' + w
+        deleteData += '\n\tAND ' + w
+
+    deleteData = 'DELETE FROM [{}].[{}]'+deleteData + ';'
+    print(deleteData)
+    engineCon(strCon).execute(
+        sat(
+            deleteData.format(schema, table)
+        ).execution_options(autocommit=True)
+    )
+
+
+def createTableStament(data, schema='dbo', table='newTable', index=False):
     columnName = data.columns.values
 
     datetype = {
@@ -73,6 +105,8 @@ def createTableStament(data, schema='dbo', table='newTable'):
         'object': 'nvarchar'
     }
     stament = ''
+    if index:
+        stament = '\t[ID_{}] int identity(1,1)'.format(table)
     for c in columnName:
 
         if stament != '':
@@ -92,10 +126,10 @@ def createTableStament(data, schema='dbo', table='newTable'):
     '''.format(schema, table, schema, table, stament)
 
 
-def createTable(srtCon, schema, table, data):
-    TableStament = createTableStament(
-        data=data, schema=schema, table=table)
-    engineCon(srtCon).execute(
+def createTable(strCon, schema, table, data, index=False):
+    TableStament = createTableStament(data, schema, table, index)
+    # print(TableStament)
+    engineCon(strCon).execute(
         sat(
             TableStament
         ).execution_options(autocommit=True)
