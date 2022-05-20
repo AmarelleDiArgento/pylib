7dfe6fe15a13919d3c0922d1c3970408e1a2ebe8
AmarelleDiArgento <ewige.quel@gmail.com>
2022-05-19 22:27:10 -0500

    remover impresion where delete

--- py_lib.py
+++ py_lib.py
@@ -596,7 +596,7 @@ def deleteDataToSql(strCon, schema, table, where=[]):
     where = where_builder(where)
 
     deleteData = 'DELETE FROM [{}].[{}]\n'+where + ';'
-    print(deleteData)
+    # print(deleteData)
     engineCon(strCon).execute(
         sat(
             deleteData.format(schema, table)
