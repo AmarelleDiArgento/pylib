95753bf31724e1fcfff495c6f09909ea7a85dfec
AmarelleDiArgento <ewige.quel@gmail.com>
2022-06-07 21:05:46 -0500

    projection add

--- py_lib.py
+++ py_lib.py
@@ -50,7 +50,6 @@ def directories(ROOT, files, directories):
                 new_directory
             )
             file[directory] = new_directory
-    # print(files)
     return files
 
 
@@ -587,8 +586,10 @@ def insertDataToSql_Alchemy(strCon, schema, table, data, truncate=False, depureC
 
         print('insert {} to {} rows'.format(total, total))
     except sql.Error as ex:  # Bad >:[
+        print(ex)
         raise ex
     except Exception as ex:  # Bad. >:[
+        print(ex)
         raise ex
 
 
@@ -603,7 +604,7 @@ def deleteDataToSql(strCon, schema, table, where=[]):
             DELETE FROM [{}].[{}]
                 {}
         '''.format(ifexist, schema, table, where)
-    print(deleteData)
+    # print(deleteData)
     engineCon(strCon).execute(
         sat(
             deleteData
@@ -612,7 +613,7 @@ def deleteDataToSql(strCon, schema, table, where=[]):
 
 
 def depure(strCon, df, schema, table, depureColumns):
-    # print(depureColumns)
+    # print(df)
     depure = df.loc[:, depureColumns].drop_duplicates(subset=depureColumns)
 
     # print(depure.shape[0])
